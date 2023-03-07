e9d37e85f1a1e1f134ce69411c9545078d3ae0da
Sunay Doğan <84284117+sunay-d@users.noreply.github.com>
2023-03-07 16:05:06 +0300

    new recipes

--- src/data.js
+++ src/data.js
@@ -17,27 +17,27 @@ const yemekListesi = [
     },
     {
         id:1,
-        ad:"Kabak Yemeği",
-        tur: "aksam",
-        resim: "https://images.unsplash.com/photo-1628369995017-aa9f540fa2c7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
-        malzeme: ["kabak", "yağ", "tuz"],
-        tarif: ["sgdg"]
+        ad:"Elmalı Tart",
+        tur: "tatli",
+        resim: "https://images.unsplash.com/photo-1568571780765-9276ac8b75a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YXBwbGUlMjBwaWV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+        malzeme: ["1 bardak Sana yağ (250 gr.)","1 su bardağı toz şeker","2 yumurta","1 kabartma tozu","un","elma","tarçın","pudra şekeri"],
+        tarif: ["Malzemeler iyice yoğrulur. (Hamur biraz sert olacak)","Hamurun yarısı ayrılıp buzluğa konur.", "Kalan hamur yağlanmış tepsi üzerine yayılır.","Harç için elma, tarçın ve şeker pişirilir.","Harç tepsiye yayılmış hamurun üzerine sürülür.","Buzluktaki hamur rendenin kalın tarafıyla tepinin üzerine rendelenir.","Tepsi fırına verilir.","Fırından çıkardıktan sonra üzerine pudra şekeri serpilir."]
     },
     {
         id:2,
         ad:"Tiramisu",
         tur: "tatli",
         resim: "https://images.unsplash.com/photo-1571877227200-a0d98ea607e9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=436&q=80",
-        malzeme: ["süt", "şeker", "yumurta"],
-        tarif: ["sgdg"]
+        malzeme: ["Keki için:","3 yumurta","1 su bardağı toz şeker","Yarım su bardağı süt","Yarım su bardağı sıvıyağ","2 yemek kaşığı kakao","1 paket kabartma tozu","1 paket vanilya","1,5 su bardağı un","Islatmak için:","1,5 su bardağı süt","2 yemek kaşığı toz şeker","1 yemek kaşığı granül kahve","Muhallebisi için:","4 su bardağı süt","1 su bardağı toz şeker","5 yemek kaşığı un","1 adet yumurta","200 gr. labne peynir","Üzeri için:","Kakao"],
+        tarif: ["Keki için bir karıştırma kabında 3 yumurta ve 1 su bardağı toz şekeri karıştırılır.","Yarım su bardağı süt ve yarım su bardağı sıvı yağ eklenir.","Elendikten sonra 1,5 su bardağı un, 1 paket kabartma tozu, 1 paket vanilya ve 2 yemek kaşığı kakao eklenir.","Bir borcam yağlanarak karışım içine aktarılır.","Karışım fırına verilerek 160 derecede 30dk pişirilir.","Muhallebi için uygun bir tencereye 4 su bardağı süt, 1 adet yumurta, 1 su bardağı toz şeker ve 5 yemek kaşığı un alınır.","Muhallebi kaynayana kadar pişirilir.","Uygun bir karıştırma kabında muhallebi ve labne peynir pürüzsüz olana kadar karıştırılır.","Keki ıslatmak için bir sos tavasında 1,5 su bardağı süt, 1 yemek kaşığı kahve, 2yemek kaşığı toz şeker, şeker eriyene kadar karıştırılır.","Pişen keki fırından çıkardıktan sonra üzerine bir çatal yardımıyla delikler açılır.","Kahveli karışım sıcak kekin üzerinde gezdirilir.","Hazırlanan muhallebi kekin üzerine boşaltılır ve üzerine yayılır.","Oda sıcaklığına gelinceye kadar bekledikten sonra üzerine kakao serpilir.","En az 2-3 saat buzdolabında bekletilir."]
     },
     {
         id:3,
-        ad:"Börek",
-        tur: "cay",
-        resim: "https://images.unsplash.com/photo-1617806501736-fc7cab7c05bf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-        malzeme: ["süt", "şeker", "yumurta"],
-        tarif: ["sgdg"]
+        ad:"Browni",
+        tur: "tatli",
+        resim: "https://images.unsplash.com/photo-1588539543889-20cc7ce4df55?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YnJvd25pZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+        malzeme: ["3 yumurta","1,5 su bardağı şeker","1 bardağa yakın ayçiçek yağı","1 bardak süt","2 bardak un","1 yemek kaşığı kakao","1 paket kabartma tozu"],
+        tarif: ["Yumurta, şeker, yağ, süt ve kakao karıştırılır.","Karışımdan bir su bardağı kenara ayrılır.","Karışımın kalanına un ve kabartma tozu katılıp karıştırılır.","Yağlanmış tepsiye dökülür.","Fırına verilir.","Fırından çıakrdıktan 2-3 dk. sonra daha önce ayrılan 1 su bardağı karışım üzerine boşaltılır.","Soğuyunca kesilip isteğe göre üzerine hindistan cevizi serpilir."]
     },
 
 ]
